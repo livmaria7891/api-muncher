81f408a0e7843f79724ae2c89c4e77cb71404433
Olivia <reach.olivialegge@gmail.com>
2016-11-06 08:28:44 -0800

    some styling and some controller tests

--- test/controllers/homepages_controller_test.rb
+++ test/controllers/homepages_controller_test.rb
@@ -6,9 +6,16 @@ class HomepagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
+  test "should get show based on URI" do
+    VCR.use_cassette("recipes") do
+      get :show, {uri: 'http://www.edamam.com/ontologies/edamam.owl#recipe_637913ec61d9da69eb451818c3293df2'}
+      assert_response :success
+      assert_template :show
+    end
+  end
+
+  test "search_results should pull list of recipes from API" do
+    
   end
 
 end
